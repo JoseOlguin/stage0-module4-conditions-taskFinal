c999a6b6308a569b6171a680a4bc7dec84cdf327
JoseOlguin <movistar.8331560462@gmail.com>
2024-07-26 17:56:25 -0600

    Commit for: stage0-module4-conditions-taskFinal

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        System.out.println(
+            x == 0 && y == 0 ? "zero"   :
+            x  > 0 && y  > 0 ? "first"  :
+            x  < 0 && y  > 0 ? "second" :
+            x  < 0 && y  < 0 ? "third"  :
+            "fourth"
+        );
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -1,7 +1,20 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import java.util.Random;
+
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        if (firstSide <= 0.0 || secondSide <= 0.0 || thirdSide <= 0.0) {
+            System.out.println("it's not a triangle");
+        } else {
+            double sumSides = firstSide + secondSide + thirdSide;
+            double maxSide = firstSide > secondSide ? (firstSide > thirdSide ? firstSide : thirdSide) : (secondSide > thirdSide ? secondSide : thirdSide);
+            double shortestTwoSides = sumSides - maxSide;
+            if (shortestTwoSides > maxSide) {
+                System.out.println("this is a valid triangle");
+            } else {
+                System.out.println("it's not a triangle");
+            }
+        }
     }
 }
